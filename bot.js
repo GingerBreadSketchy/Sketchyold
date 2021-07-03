4b0a22f080df6b1e7f43e43607f35e1994fc7a71
agentnox <86611458+agentnox@users.noreply.github.com>
2021-07-03 18:01:43 +0530

    🔧 Fixed configure

--- bot.js
+++ bot.js
@@ -259,16 +259,18 @@ ${chalk.blue.italic('ℹ️ WhatsApp වෙත සම්බන්ධ වෙම�
 
         if (msg.messageStubType === 32 || msg.messageStubType === 28) {
             // see you message
-            var gb = await getMessage(msg.key.remoteJid, 'goodbye');
-            var blogo = await axios.get (Config.BYE_LOGO, {responseType: 'arraybuffer'});
+            var blogo = await axios.get (Config.BYE_LOGO, {responseType: 'arraybuffer'})
+            var gb = await getMessage(msg.key.remoteJid, 'goodbye')
+            
             if (gb !== false) {
                 await conn.sendMessage(msg.key.remoteJid, Buffer.from (blogo.data), MessageType.video, {mimetype: Mimetype.gif, caption: gb.message});
             }
             return;
         } else if (msg.messageStubType === 27 || msg.messageStubType === 31) {
             // Welcome message
-            var gb = await getMessage(msg.key.remoteJid);
-            var wlogo = await axios.get (Config.WELCOME_LOGO, {responseType: 'arraybuffer'});
+            var wlogo = await axios.get (Config.WELCOME_LOGO, {responseType: 'arraybuffer'})
+            var gb = await getMessage(msg.key.remoteJid)
+            
             if (gb !== false) {
                 await conn.sendMessage(msg.key.remoteJid, Buffer.from (wlogo.data), MessageType.video, {mimetype: Mimetype.gif, caption: gb.message});
             }
