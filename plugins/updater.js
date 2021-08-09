81b7dcebbe5bcfdb37269fa9214937a8786c63f0
Achintha Bandara <wmachintha@gmail.com>
2021-08-09 13:25:10 +0530

    Verification System

--- plugins/updater.js
+++ plugins/updater.js
@@ -96,3 +96,57 @@ Amdi.applyCMD({pattern: 'update now$', fromMe: true,  deleteCommand: false,  des
         }
     }
 }));
+
+Amdi.applyCMD({pattern: 'verify$', fromMe: true, desc: Lang.VERIFY_DESC, dontAddCommandList: true}, (async (message, match) => {
+    await git.fetch();
+    var commits = await git.log([Config.BRANCH + '..origin/' + Config.BRANCH]);
+    if (commits.total === 0) {
+        return await message.client.sendMessage(
+            message.jid,
+            Lang.VERIFY, MessageType.text
+        );    
+    } else {
+        var guncelleme = await message.reply(Lang.VERIFYING);
+        if (Config.HEROKU.HEROKU) {
+            try {
+                var app = await heroku.get('/apps/' + Config.HEROKU.APP_NAME)
+            } catch {
+                await message.client.sendMessage(
+                    message.jid,Lang.INVALID_HEROKU, MessageType.text);
+                await new Promise(r => setTimeout(r, 1000));
+                return await message.client.sendMessage(
+                    message.jid,Lang.IN_AF, MessageType.text);
+            }
+
+            git.fetch('upstream', Config.BRANCH);
+            git.reset('hard', ['FETCH_HEAD']);
+
+            var git_url = app.git_url.replace(
+                "https://", "https://api:" + Config.HEROKU.API_KEY + "@"
+            )
+            
+            try {
+                await git.addRemote('heroku', git_url);
+            } catch { console.log('heroku remote ekli'); }
+            await git.push('heroku', Config.BRANCH);
+
+            await message.client.sendMessage(
+                message.jid,Lang.VERIFIED, MessageType.text);
+
+            await message.sendMessage(Lang.AFTER_UPDATE);
+            
+        } else {
+            git.pull((async (err, update) => {
+                if(update && update.summary.changes) {
+                    await message.client.sendMessage(
+                        message.jid,Lang.VERIFIED_LOCAL, MessageType.text);
+                    exec('npm install').stderr.pipe(process.stderr);
+                } else if (err) {
+                    await message.client.sendMessage(
+                        message.jid,'*❌ Verification faield!*\n*Error:* ```' + err + '```', MessageType.text);
+                }
+            }));
+            await guncelleme.delete();
+        }
+    }
+}));
