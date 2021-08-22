3c07f871cdd9d5525088a833d98c170590e95510
DARK CRIME <86085229+DARKCRIME1@users.noreply.github.com>
2021-08-22 19:14:01 +0530

    Update events.js

--- events.js
+++ events.js
@@ -1,10 +1,4 @@
-/* Copyright (C) 2020 Black Amda.
 
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
-
-QueenAmdi - Black Amda
-*/
 
 // Komutları burada tutacağız.
 var config = require('./config');
