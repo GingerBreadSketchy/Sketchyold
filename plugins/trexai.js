90fe2ae4593156e0b3939eebb759614c4a5d4940
DARK CRIME <86085229+DARKCRIME1@users.noreply.github.com>
2021-08-22 10:14:22 +0530

    Create trexai.js

---
+++ plugins/trexai.js
@@ -0,0 +1,99 @@
+const QueenSew = require('../events');
+const ffmpeg = require('fluent-ffmpeg');
+const fs = require('fs');
+const https = require('https');
+const googleTTS = require('google-translate-tts');
+const { MessageType, Mimetype, MessageOptions } = require('@adiwajshing/baileys');
+const Language = require('../language');
+const Lang = Language.getString('voicy');
+const Config = require('../config');
+const axios = require('axios')
+const axiosdef = require("axios").default;
+const os = require('os')
+const translatte = require('translatte');
+const LanguageDetect = require('languagedetect');
+const lngDetector = new LanguageDetect();
+const Heroku = require('heroku-client');
+const heroku = new Heroku({
+    token: Config.HEROKU.API_KEY
+});
+let baseURI = '/apps/' + Config.HEROKU.APP_NAME;
+
+let wk = Config.WORKTYPE == 'public' ? false : true
+var vtalk_dsc = ''
+var reply_eva = ''
+
+const recognizeAudio = () => {
+    const headers = new Headers({
+        'Content-Type': 'audio/wav',
+        "Authorization": `Bearer ${Config.WITAI_API}`,
+        'Cache-Control': 'no-cache',
+        'Transfer-Encoding': 'chunked'
+    })
+    const requestBody = {
+        method: "POST",
+        body: fs.readFileSync('output.wav'),
+        headers: headers
+    }
+    return fetch("https://api.wit.ai/speech?v=20200219", requestBody)
+        .then(response => response.lasijson())
+        .then(lasijson => lasijson._text)
+}
+const convertToWav = file => {
+    return ffmpeg(file)
+        .audioCodec('pcm_s16le')
+        .format('wav')
+        .save('output.wav')
+}
+
+QueenSew.applyCMD({on: 'text', fromMe: wk, dontAdCommandList: true, delownsewcmd: false}, (async (message, match) => {
+    if (message.message.startsWith('Rex') && Config.FULLSEW !== 'true') {        
+        var unique_ident = message.client.user.jid.split('@')[0]      
+        var finm = message.message.replace('Rex', '').replace(' ', '')   
+        var ldet = lngDetector.detect(finm)
+        var trmsg = finm
+
+        var uren = encodeURI(trmsg)
+        await axios.get('http://api.brainshop.ai/get?bid=157582&key=HzYiQOTcF1zOB0cs&uid=' + unique_ident + '&msg=' + uren).then(async (response) => {
+            var fins = ''                           
+            if (conf.LANG !== 'EN') {
+                ceviri = await translatte(response.data.cnt, {from: 'auto', to: conf.LANG});
+                if ('text' in ceviri) {
+                    fins = ceviri.text
+                }
+            } else { fins = response.data.cnt }
+            await message.client.sendMessage(message.jid,fins, MessageType.text, { quoted: message.data})
+        })
+    }
+}));
+
+
+QueenSew.applyCMD({ pattern: 'fullrex ?(.*)', desc: fulleva_dsc, fromMe: true, usage: '.fullsew on / off' }, (async (message, match) => {
+    var eva_status = `${conf.FULLEVA}`
+    if (match[1] == 'on') {
+        if (eva_status == 'true') {
+            return await message.client.sendMessage(message.jid, '*' + already_on + '*', MessageType.text)
+        }
+        else {
+            await heroku.patch(baseURI + '/config-vars', { 
+                body: { 
+                    ['FULL_SEW']: 'true'
+                } 
+            });
+            await message.client.sendMessage(message.jid, '*' + succ_on + '*', MessageType.text)
+        }
+    }
+    else if (match[1] == 'off') {
+        if (eva_status !== 'true') {
+            return await message.client.sendMessage(message.jid, '*' + already_off + '*', MessageType.text)
+        }
+        else {
+            await heroku.patch(baseURI + '/config-vars', { 
+                body: { 
+                    ['FULL_SEW']: 'false'
+                } 
+            });
+            await message.client.sendMessage(message.jid, '*' + succ_off + '*', MessageType.text)
+        }
+    }
+}));
