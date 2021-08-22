14b9767c28c1a8e036d99db0c310562f9ce32668
DARK CRIME <86085229+DARKCRIME1@users.noreply.github.com>
2021-08-22 19:36:30 +0530

    Update tgmtgn.js

--- plugins/tgmtgn.js
+++ plugins/tgmtgn.js
@@ -7,7 +7,7 @@ const Config = require('../config');
 
 
 if (Config.WORKTYPE == 'private') {
-XTroid.applyCMD({pattern: 'gm', fromMe: true, deleteCommand: true, desc: GM,}, (async (message, match) => {
+XTroid.applyCMD({pattern: 'tgm', fromMe: true, deleteCommand: true, desc: GM,}, (async (message, match) => {
 
     var r_text = new Array ();
 
@@ -28,7 +28,7 @@ XTroid.applyCMD({pattern: 'gm', fromMe: true, deleteCommand: true, desc: GM,}, (
     }));
 
 
-XTroid.applyCMD({pattern: 'gn', fromMe: true, deleteCommand: true, desc: GN,}, (async (message, match) => {
+XTroid.applyCMD({pattern: 'tgn', fromMe: true, deleteCommand: true, desc: GN,}, (async (message, match) => {
 
         var r_text = new Array ();
     
@@ -51,7 +51,7 @@ XTroid.applyCMD({pattern: 'gn', fromMe: true, deleteCommand: true, desc: GN,}, (
     
 
     if (Config.WORKTYPE == 'public') {
-        XTroid.applyCMD({pattern: 'gm', fromMe: false, deleteCommand: true, desc: GM,}, (async (message, match) => {
+        XTroid.applyCMD({pattern: 'tgm', fromMe: false, deleteCommand: true, desc: GM,}, (async (message, match) => {
         
             var r_text = new Array ();
         
@@ -72,7 +72,7 @@ XTroid.applyCMD({pattern: 'gn', fromMe: true, deleteCommand: true, desc: GN,}, (
             }));
         
         
-        XTroid.applyCMD({pattern: 'gn', fromMe: false, deleteCommand: true, desc: GN,}, (async (message, match) => {
+        XTroid.applyCMD({pattern: 'tgn', fromMe: false, deleteCommand: true, desc: GN,}, (async (message, match) => {
         
                 var r_text = new Array ();
             
@@ -90,7 +90,7 @@ XTroid.applyCMD({pattern: 'gn', fromMe: true, deleteCommand: true, desc: GN,}, (
                     message.jid,(r_text[i]), MessageType.text);
             
                 }));    
-                XTroid.applyCMD({pattern: 'gm', fromMe: true, deleteCommand: true,dontAddCMDList: true }, (async (message, match) => {
+                XTroid.applyCMD({pattern: 'tgm', fromMe: true, deleteCommand: true,dontAddCMDList: true }, (async (message, match) => {
         
                     var r_text = new Array ();
                 
@@ -111,7 +111,7 @@ XTroid.applyCMD({pattern: 'gn', fromMe: true, deleteCommand: true, desc: GN,}, (
                     }));
                 
                 
-                XTroid.applyCMD({pattern: 'gn', fromMe: true, deleteCommand: true,dontAddCMDList: true }, (async (message, match) => {
+                XTroid.applyCMD({pattern: 'tgn', fromMe: true, deleteCommand: true,dontAddCMDList: true }, (async (message, match) => {
                 
                         var r_text = new Array ();
                     
