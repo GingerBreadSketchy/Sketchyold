859baf2fadb08ea4f71c5be487e3f23fa2fe0428
DARK CRIME <86085229+DARKCRIME1@users.noreply.github.com>
2021-08-28 05:54:40 +0530

    [+] - ADDED ANIME

--- plugins/anime.js
+++ plugins/anime.js
@@ -93,7 +93,7 @@ if (Config.WORKTYPE == 'private') {
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: 'Made by *❰🍁🔱  T Rex BOT  🔱🍁❱*'})
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: '*❰🍁🔱  T Rex BOT  🔱🍁❱*'})
 
     }));
 }
@@ -187,7 +187,7 @@ else if (Config.WORKTYPE == 'public') {
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: 'Made by *❰🍁🔱  T Rex BOT  🔱🍁❱*'})
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: '*❰🍁🔱  T Rex BOT  🔱🍁❱*'})
 
     }));
 }
