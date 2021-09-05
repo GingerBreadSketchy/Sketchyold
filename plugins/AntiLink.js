0546501131d160d6464d84170200244eb0490e05
DARK CRIME <86085229+DARKCRIME1@users.noreply.github.com>
2021-09-05 16:09:36 +0530

    Update AntiLink.js

--- plugins/AntiLink.js
+++ plugins/AntiLink.js
@@ -1,9 +1,4 @@
-/* Copyright (C) 2021 Queen Amdi.
 
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
-
-Queen Amdi - Black Amda*/
 
 const Amdi = require('../events');
 const {MessageType} = require('@adiwajshing/baileys');
