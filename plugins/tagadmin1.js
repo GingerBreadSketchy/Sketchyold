d147ad3bfa5f86cc39fd19db9c6c17269617460f
DARK CRIME <86085229+DARKCRIME1@users.noreply.github.com>
2021-09-06 17:52:31 +0530

    Create tagadmin1.js

---
+++ plugins/tagadmin1.js
@@ -0,0 +1,87 @@
+const QueenSew = require('../events');
+
+const {MessageType} = require('@adiwajshing/baileys');
+
+const Config = require('../config');
+
+const Language = require('../language');
+
+if (Config.WORKTYPE == 'private') {
+
+    QueenSew.applyCMD({pattern: 'tagadmin', fromMe: true }, (async (message, match) => {
+
+        let grup = await message.client.groupMetadata(message.jid);
+
+        var jids = [];
+
+        mesaj = '';
+
+        grup['participants'].map(async (uye) => {
+
+            if (uye.isAdmin) {
+
+                mesaj += '💠 @' + uye.id.split('@')[0] + '\n▫️👑 wa.me/'+ uye.id.split('@')[0] + '  👑\n\n';
+
+                jids.push(uye.id.replace('c.us', 's.whatsapp.net'));
+
+            }
+
+        });
+
+        await message.client.sendMessage(message.jid,'◻    🔱    *👑 Group Admins 👑*    🔱    ◻\n\n\n' + mesaj, MessageType.extendedText, {contextInfo: {mentionedJid: jids}, previewType: 0})
+
+    }));
+
+}
+
+else if (Config.WORKTYPE == 'public') {
+
+    QueenSew.applyCMD({pattern: 'tagadmin', fromMe: false, desc: Lang.CONTADMİN}, (async (message, match) => {
+
+        let grup = await message.client.groupMetadata(message.jid);
+
+        var jids = [];
+
+        mesaj = '';
+
+        grup['participants'].map(async (uye) => {
+
+            if (uye.isAdmin) {
+
+                mesaj += '💠 @' + uye.id.split('@')[0] + '\n▫️👑 wa.me/' + uye.id.split('@')[0] + '  👑\n\n';
+
+                jids.push(uye.id.replace('c.us', 's.whatsapp.net'));
+
+            }
+
+        });
+
+        await message.client.sendMessage(message.jid,'◻    🔱    *👑 Group Admins 👑*    🔱    ◻\n\n\n' + mesaj, MessageType.extendedText, {contextInfo: {mentionedJid: jids}, previewType: 0})
+
+    }));
+
+    QueenSew.applyCMD({pattern: 'tagadmin', fromMe: true, desc: Lang.CONTADMİN, dontAdCommandList: true}, (async (message, match) => {
+
+        let grup = await message.client.groupMetadata(message.jid);
+
+        var jids = [];
+
+        mesaj = '';
+
+        grup['participants'].map(async (uye) => {
+
+            if (uye.isAdmin) {
+
+                mesaj += '💠 @' + uye.id.split('@')[0] + '\n▫️👑 wa.me/' + uye.id.split('@')[0] + '  👑\n\n';
+
+                jids.push(uye.id.replace('c.us', 's.whatsapp.net'));
+
+            }
+
+        });
+
+        await message.client.sendMessage(message.jid,'◻    🔱    *👑 Group Admins 👑*    🔱    ◻\n\n\n' + mesaj, MessageType.extendedText, {contextInfo: {mentionedJid: jids}, previewType: 0})
+
+    }));
+
+}
