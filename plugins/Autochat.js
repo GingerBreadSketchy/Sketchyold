eacc00c42098964eb95e770e8a6933ad4ebd785d
DARK CRIME <86085229+DARKCRIME1@users.noreply.github.com>
2021-09-06 13:44:14 +0530

    Update Autochat.js

--- plugins/Autochat.js
+++ plugins/Autochat.js
@@ -35,177 +35,19 @@ const ADD = '94704029407,94788306130'
 
 var kickmsg = ''
 
-if (Config.LANG == 'SI') kickmsg = '_*❌ ලින්ක් bot අයිතිකරු විසින් තහනම් කර ඇත.*_\n\n ❰🍁🔱  T Rex BOT  🔱🍁❱  '
+if (Config.LANG == 'SI') kickmsg = '_*හායි කෝමද ඔයාට\n\n ❰🍁🔱  T Rex BOT  🔱🍁❱  '
 
 if (Config.LANG == 'EN') kickmsg = '_*❌ Links are prohibited by the bot owner.*_\n\n ❰🍁🔱  T Rex BOT  🔱🍁❱  '
 
 Amdi.applyCMD({on: 'text', fromMe: false,  deleteCommand: false}, (async (message, match) => {    
 
-        let getword1bad = new RegExp('https://')
-
-        let getword2bad = new RegExp('.com')
-
-        let getword3bad = new RegExp('.lk')
-
-        let getword4bad = new RegExp('hi')
-
-        let getword5bad = new RegExp('http://')
-
-        let getword6bad = new RegExp('http')
-
-        let getword7bad = new RegExp('https')
-
-        let getword8bad = new RegExp('www.')
-
-        let getword9bad = new RegExp('WWW.')
-
-        let getword10bad = new RegExp('youtube.com')
-        
+        let getword1bad = new RegExp('hi')
         
         
         
         if (getword1bad.test(message.message)) {
 
-            var us = await checkUsAdmin(message)
-
-            var im = await checkImAdmin(message)
-
-            if (!im) return;
-
-            if (us) return;
-
-            await message.client.sendMessage(message.jid,kickmsg, MessageType.text, {quoted: message.data })
-
-            await message.client.groupRemove(message.jid, [message.data.participant]);         
-
-        } 
-      
-      else if (getword2bad.test(message.message)) {
-
-            var us = await checkUsAdmin(message)
-
-            var im = await checkImAdmin(message)
-
-            if (!im) return;
-
-            if (us) return;
-
-            await message.client.sendMessage(message.jid,kickmsg, MessageType.text, {quoted: message.data })
-
-            await message.client.groupRemove(message.jid, [message.data.participant]);         
-
-        } 
-      
-      else if (getword3bad.test(message.message)) {
-
-            var us = await checkUsAdmin(message)
-
-            var im = await checkImAdmin(message)
-
-            if (!im) return;
-
-            if (us) return;
-
-            await message.client.sendMessage(message.jid,kickmsg, MessageType.text, {quoted: message.data })
-
-            await message.client.groupRemove(message.jid, [message.data.participant]);         
-
-        } 
-
-      else if (getword4bad.test(message.message)) {
-            await message.client.sendMessage(message.jid,kickmsg, MessageType.text, {quoted: message.data })
-
-            await message.client.groupRemove(message.jid, [message.data.participant]);         
-
-        } 
-      else if (getword5bad.test(message.message)) {
-
-            var us = await checkUsAdmin(message)
-
-            var im = await checkImAdmin(message)
-
-            if (!im) return;
-
-            if (us) return;
-
-            await message.client.sendMessage(message.jid,kickmsg, MessageType.text, {quoted: message.data })
-
-            await message.client.groupRemove(message.jid, [message.data.participant]);         
-
-        } 
-      else if (getword6bad.test(message.message)) {
-
-            var us = await checkUsAdmin(message)
-
-            var im = await checkImAdmin(message)
-
-            if (!im) return;
-
-            if (us) return;
-
-            await message.client.sendMessage(message.jid,kickmsg, MessageType.text, {quoted: message.data })
-
-            await message.client.groupRemove(message.jid, [message.data.participant]);         
-
-        } 
-      else if (getword7bad.test(message.message)) {
-
-            var us = await checkUsAdmin(message)
-
-            var im = await checkImAdmin(message)
-
-            if (!im) return;
-
-            if (us) return;
-
-            await message.client.sendMessage(message.jid,kickmsg, MessageType.text, {quoted: message.data })
-
-            await message.client.groupRemove(message.jid, [message.data.participant]);         
-
-        } 
-      else if (getword8bad.test(message.message)) {
-
-            var us = await checkUsAdmin(message)
-
-            var im = await checkImAdmin(message)
-
-            if (!im) return;
-
-            if (us) return;
-
-            await message.client.sendMessage(message.jid,kickmsg, MessageType.text, {quoted: message.data })
-
-            await message.client.groupRemove(message.jid, [message.data.participant]);         
-
-        } 
-      else if (getword9bad.test(message.message)) {
-
-            var us = await checkUsAdmin(message)
-
-            var im = await checkImAdmin(message)
-
-            if (!im) return;
-
-            if (us) return;
-
-            await message.client.sendMessage(message.jid,kickmsg, MessageType.text, {quoted: message.data })
-
-            await message.client.groupRemove(message.jid, [message.data.participant]);         
-
-        } 
-      else if (getword10bad.test(message.message)) {
-
-            var us = await checkUsAdmin(message)
-
-            var im = await checkImAdmin(message)
-
-            if (!im) return;
-
-            if (us) return;
-
-            await message.client.sendMessage(message.jid,kickmsg, MessageType.text, {quoted: message.data })
-
-            await message.client.groupRemove(message.jid, [message.data.participant]);         
+            await message.client.sendMessage(message.jid,kickmsg, MessageType.text, {quoted: message.data })     
 
         } 
 
