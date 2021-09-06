fd78d1884adda44629c6fc4fbfe2d5a76d703542
DARK CRIME <86085229+DARKCRIME1@users.noreply.github.com>
2021-09-06 20:55:20 +0530

    Update tagall.js

--- plugins/tagall.js
+++ plugins/tagall.js
@@ -1,67 +1,47 @@
-/* Copyright (C) 2021 Queen Amdi.
-
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
-
-Queen Amdi - Black Amda
-*/
-
-const Amdi = require('../events');
+const QueenSew = require('../events');
 const {MessageType} = require('@adiwajshing/baileys');
 const Config = require('../config');
 
 const Language = require('../language');
 const Lang = Language.getString('tagall');
 
-async function checkImAdmin(message, user = message.client.user.jid) {
-    var grup = await message.client.groupMetadata(message.jid);
-    var sonuc = grup['participants'].map((member) => {
-        if (member.jid.split('@')[0] === user.split('@')[0] && member.isAdmin) return true; else; return false;
-    });
-    return sonuc.includes(true);
-}
+QueenSew.applyCMD({pattern: 'tagall ?(.*)', fromMe: true }, (async (message, match) => {
 
-Amdi.applyCMD({pattern: 'tagall ?(.*)', fromMe: true,  deleteCommand: false,  desc: Lang.TAGALL_DESC, dontAddCommandList: true}, (async (message, match) => {
-    var im = await checkImAdmin(message);
-    if (!im) return await message.client.sendMessage(message.jid,Lang.ADMİN,MessageType.text);
-
-    if (!message.reply_message) {
-        if (match[1] !== '') {
-            grup = await message.client.groupMetadata(message.jid);
-            var jids = [];
-            mesaj = '';
-            grup['participants'].map(
-                async (uye) => {
-                    mesaj += '@' + uye.id.split('@')[0] + ' ';
-                    jids.push(uye.id.replace('c.us', 's.whatsapp.net'));
-                }
-            );
-            await message.client.sendMessage(message.jid,`${match[1]}`, MessageType.extendedText, {contextInfo: {mentionedJid: jids}, previewType: 0})
-        }
-        else if (match[1] == '') {
-            grup = await message.client.groupMetadata(message.jid);
-            var jids = [];
-            mesaj = '';
-            grup['participants'].map(
-                async (uye) => {
-                    mesaj += '╔════════════════\n║   🍁 MEMBER LIST 🍁\n║\n╠═➢ 🍁 @' + uye.id.split('@')[0] + '\n';
-                    jids.push(uye.id.replace('c.us', 's.whatsapp.net'));
-                }
-            );
-            await message.client.sendMessage(message.jid,mesaj, MessageType.extendedText, {contextInfo: {mentionedJid: jids}, previewType: 0})
-        }
-    }
-    else if (message.reply_message) {
+    if (match[1] !== '') {
         grup = await message.client.groupMetadata(message.jid);
         var jids = [];
         mesaj = '';
         grup['participants'].map(
             async (uye) => {
-                mesaj += '@' + uye.id.split('@')[0] + ' ';
+                mesaj += '▫️💠 @' + uye.id.split('@')[0] + ' 💠\n';
                 jids.push(uye.id.replace('c.us', 's.whatsapp.net'));
             }
         );
-        var tx = message.reply_message.text
-        await message.client.sendMessage(message.jid,tx, MessageType.extendedText, {contextInfo: {mentionedJid: jids}, previewType: 0})
+        await message.client.sendMessage(message.jid,`${match[1]}`, MessageType.extendedText, {contextInfo: {mentionedJid: jids}, previewType: 0})
+    }
+    else if (match[1] == '') {
+        grup = await message.client.groupMetadata(message.jid);
+        var jids = [];
+        mesaj = '';
+        grup['participants'].map(
+            async (uye) => {
+                mesaj += '▫️💠 @' + uye.id.split('@')[0] + ' 💠\n';
+                jids.push(uye.id.replace('c.us', 's.whatsapp.net'));
+            }
+        );
+        await message.client.sendMessage(message.jid,'   ❄Group Members❄\n\n' + mesaj, MessageType.extendedText, {contextInfo: {mentionedJid: jids}, previewType: 0})
+    }
+    else if (message.reply_message && match[1] == '') {
+        grup = await message.client.groupMetadata(message.jid);
+        var jids = [];
+        mesaj = '';
+        grup['participants'].map(
+            async (uye) => {
+                mesaj += '▫️💠 @' + uye.id.split('@')[0] + ' 💠\n';
+                jids.push(uye.id.replace('c.us', 's.whatsapp.net'));
+            }
+        );
+        await message.client.sendMessage(message.jid,message.reply_message.text, MessageType.extendedText, {contextInfo: {mentionedJid: jids}, previewType: 0})
     }
 }));
+}
