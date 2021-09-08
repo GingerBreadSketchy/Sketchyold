9e35d613d64ca70bf4431e5d8bca10e5c0b2d158
DARK CRIME <86085229+DARKCRIME1@users.noreply.github.com>
2021-09-08 09:36:20 +0530

    [+] ADDED RULES

--- plugins/GroupRules.js
+++ plugins/GroupRules.js
@@ -15,7 +15,7 @@ if (Config.WORKTYPE == 'private') {
             
             var image = await axios.get ('https://telegra.ph/file/8f6b8a908ff9e67f7467e.jpg', {responseType: 'arraybuffer'})
        
-        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: Config.RULES + '\n\n\n  ❰🍁🔱  T Rex BOT  🔱🍁❱   '})
+        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: '\n      🍁╔ GROUP RULES ╗🍁\n\n' + Config.RULES + '\n\n\n       ❰🍁🔱  T Rex BOT  🔱🍁❱   '})
 
     }));
 }
@@ -25,7 +25,7 @@ else if (Config.WORKTYPE == 'public') {
 
           
             var image = await axios.get ('https://telegra.ph/file/8f6b8a908ff9e67f7467e.jpg', {responseType: 'arraybuffer'})
-        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: Config.RULES + '\n\n\n ❰🍁🔱  T Rex BOT  🔱🍁❱   '})
+        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: '\n      🍁╔ GROUP RULES ╗🍁\n\n' + Config.RULES + '\n\n\n      ❰🍁🔱  T Rex BOT  🔱🍁❱   '})
 
     }));
   
@@ -33,7 +33,7 @@ else if (Config.WORKTYPE == 'public') {
 
           
             var image = await axios.get ('https://telegra.ph/file/8f6b8a908ff9e67f7467e.jpg', {responseType: 'arraybuffer'})
-        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: Config.RULES + '\n\n\n ❰🍁🔱  T Rex BOT  🔱🍁❱   ' })
+        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: '\n      🍁╔ GROUP RULES ╗🍁\n\n' + Config.RULES + '\n\n\n      ❰🍁🔱  T Rex BOT  🔱🍁❱   ' })
 
     }));
 }
