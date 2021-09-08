ef4fc74d9f6dfd8bfc7f8dd27e0eb2edf8f14467
DARK CRIME <86085229+DARKCRIME1@users.noreply.github.com>
2021-09-08 11:44:28 +0530

    Update love.js

--- plugins/love.js
+++ plugins/love.js
@@ -182,6 +182,69 @@ const Ln = "  *Send Love Messages.. , ආදර පණිවිඩ යැවී�
 ╚═════════════════════╝` })
 
     }));
+
+  Asena.applyCMD({pattern: 'love you', fromMe: false }, (async (message, match) => {
+
+    var r_text = new Array ();
+
+    r_text[0] = "https://telegra.ph/file/9fdcba7310b4accd41119.jpg";
+
+    r_text[1] = "https://telegra.ph/file/c91ce55d58336bee137a0.jpg";
+
+    r_text[2] = "https://telegra.ph/file/01479ee5aa5964989bf30.jpg";
+
+    r_text[3] = "https://telegra.ph/file/84f1b008c175b1dc3bcb5.jpg";
+
+    r_text[4] = "https://telegra.ph/file/7aeaf5907f67f2cf9078c.jpg";
+
+    r_text[5] = "https://telegra.ph/file/7809664f52214ec6eba99.jpg";
+
+    r_text[6] = "https://telegra.ph/file/0e96fae15204751https://telegra.ph/file/72ba4b1a8caf38604c18c.jpg24d275.jpg";
+
+    var i = Math.floor(8*Math.random())
+
+    var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer'})
+
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `╔══╗╔╗  ♡
+╚╗╔╝║║╔═╦╦╦╔╗
+╔╝╚╗║╚╣║║║║╔╣
+╚══╝╚═╩═╩═╩═╝
+
+ஜ۩۞۩ஜ YOU ஜ۩۞۩ஜ` })
+
+    }));
+
+  Asena.applyCMD({pattern: 'miss you', fromMe: false }, (async (message, match) => {
+
+    var r_text = new Array ();
+
+    r_text[0] = "https://telegra.ph/file/420aad8a5c96b545ddf52.jpg";
+
+    r_text[1] = "https://telegra.ph/file/7af1f81554120ef582fa1.jpg";
+
+    r_text[2] = "https://telegra.ph/file/f2c9064c9afc912d58a88.jpg";
+
+    r_text[3] = "https://telegra.ph/file/506cbe50b126155882d2e.jpg";
+
+    r_text[4] = "https://telegra.ph/file/7af1f81554120ef582fa1.jpg";
+
+    r_text[5] = "https://telegra.ph/file/f2c9064c9afc912d58a88.jpg";
+
+    r_text[6] = "https://telegra.ph/file/420aad8a5c96b545ddf52.jpg";
+
+    var i = Math.floor(8*Math.random())
+
+    var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer'})
+
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `╔═╦═╦╦═╦═╗╔═╦╗
+║║║║╠╣═╣═╣╚╗║╠═╦╦╗
+║║║║║╠═╠═║╔╩╗║╬║║║
+╚╩═╩╩╩═╩═╝╚══╩═╩═╝` })
+
+    }));
+ }
+
+else if (Config.WORKTYPE == 'private') {
   Asena.applyCMD({pattern:  'ffpack', fromMe: true }, (async (message, match) => {
 
     var r_text = new Array ();
@@ -313,63 +376,4 @@ const Ln = "  *Send Love Messages.. , ආදර පණිවිඩ යැවී�
 ╚═════════════════════╝` })
    
    }));
-  Asena.applyCMD({pattern: 'love you', fromMe: false }, (async (message, match) => {
-
-    var r_text = new Array ();
-
-    r_text[0] = "https://telegra.ph/file/9fdcba7310b4accd41119.jpg";
-
-    r_text[1] = "https://telegra.ph/file/c91ce55d58336bee137a0.jpg";
-
-    r_text[2] = "https://telegra.ph/file/01479ee5aa5964989bf30.jpg";
-
-    r_text[3] = "https://telegra.ph/file/84f1b008c175b1dc3bcb5.jpg";
-
-    r_text[4] = "https://telegra.ph/file/7aeaf5907f67f2cf9078c.jpg";
-
-    r_text[5] = "https://telegra.ph/file/7809664f52214ec6eba99.jpg";
-
-    r_text[6] = "https://telegra.ph/file/0e96fae15204751https://telegra.ph/file/72ba4b1a8caf38604c18c.jpg24d275.jpg";
-
-    var i = Math.floor(8*Math.random())
-
-    var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer'})
-
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `╔══╗╔╗  ♡
-╚╗╔╝║║╔═╦╦╦╔╗
-╔╝╚╗║╚╣║║║║╔╣
-╚══╝╚═╩═╩═╩═╝
-
-ஜ۩۞۩ஜ YOU ஜ۩۞۩ஜ` })
-
-    }));
-
-  Asena.applyCMD({pattern: 'miss you', fromMe: false }, (async (message, match) => {
-
-    var r_text = new Array ();
-
-    r_text[0] = "https://telegra.ph/file/420aad8a5c96b545ddf52.jpg";
-
-    r_text[1] = "https://telegra.ph/file/7af1f81554120ef582fa1.jpg";
-
-    r_text[2] = "https://telegra.ph/file/f2c9064c9afc912d58a88.jpg";
-
-    r_text[3] = "https://telegra.ph/file/506cbe50b126155882d2e.jpg";
-
-    r_text[4] = "https://telegra.ph/file/7af1f81554120ef582fa1.jpg";
-
-    r_text[5] = "https://telegra.ph/file/f2c9064c9afc912d58a88.jpg";
-
-    r_text[6] = "https://telegra.ph/file/420aad8a5c96b545ddf52.jpg";
-
-    var i = Math.floor(8*Math.random())
-
-    var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer'})
-
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `╔═╦═╦╦═╦═╗╔═╦╗
-║║║║╠╣═╣═╣╚╗║╠═╦╦╗
-║║║║║╠═╠═║╔╩╗║╬║║║
-╚╩═╩╩╩═╩═╝╚══╩═╩═╝` })
-
-    }));
  }
