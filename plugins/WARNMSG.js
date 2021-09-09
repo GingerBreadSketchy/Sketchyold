f671fb6ad26c6cca64ee327879c733fcfb4e2497
DARK CRIME <86085229+DARKCRIME1@users.noreply.github.com>
2021-09-09 16:12:15 +0530

    [+] ADDED WARN MESSAGES 📛

--- plugins/WARNMSG.js
+++ plugins/WARNMSG.js
@@ -12,28 +12,28 @@ if (Config.WORKTYPE == 'private') {
             
             var image = await axios.get ('https://telegra.ph/file/8f6b8a908ff9e67f7467e.jpg', {responseType: 'arraybuffer'})
        
-        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: '\n      📛╔ GROUP WARNS ╗📛\n\n' + Config.WARN1 + '\n\n\n       ❰🍁🔱  T Rex BOT  🔱🍁❱   '})
+        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: '\n      📛╔ GROUP WARN ╗📛\n\n' + Config.WARN1 + '\n\n\n       ❰🍁🔱  T Rex BOT  🔱🍁❱   '})
 
     }));
   Amdi.applyCMD({pattern: 'warn2', fromMe: true,  deleteCommand: false, onlyGroup: true }, (async (message, match) => {
 
           
             var image = await axios.get ('https://telegra.ph/file/8f6b8a908ff9e67f7467e.jpg', {responseType: 'arraybuffer'})
-        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: '\n      📛╔ GROUP WARNS ╗📛\n\n' + Config.WARN2 + '\n\n\n      ❰🍁🔱  T Rex BOT  🔱🍁❱   '})
+        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: '\n      📛╔ GROUP WARN ╗📛\n\n' + Config.WARN2 + '\n\n\n      ❰🍁🔱  T Rex BOT  🔱🍁❱   '})
 
     }));
   Amdi.applyCMD({pattern: 'warn3', fromMe: true,  deleteCommand: false, onlyGroup: true }, (async (message, match) => {
 
           
             var image = await axios.get ('https://telegra.ph/file/8f6b8a908ff9e67f7467e.jpg', {responseType: 'arraybuffer'})
-        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: '\n      📛╔ GROUP WARNS ╗📛\n\n' + Config.WARN3 + '\n\n\n      ❰🍁🔱  T Rex BOT  🔱🍁❱   '})
+        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: '\n      📛╔ GROUP WARN ╗📛\n\n' + Config.WARN3 + '\n\n\n      ❰🍁🔱  T Rex BOT  🔱🍁❱   '})
 
     }));
   Amdi.applyCMD({pattern: 'warn4', fromMe: true,  deleteCommand: false, onlyGroup: true }, (async (message, match) => {
 
           
             var image = await axios.get ('https://telegra.ph/file/8f6b8a908ff9e67f7467e.jpg', {responseType: 'arraybuffer'})
-        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: '\n      📛╔ GROUP WARNS ╗📛\n\n' + Config.WARN4 + '\n\n\n      ❰🍁🔱  T Rex BOT  🔱🍁❱   '})
+        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: '\n      📛╔ GROUP WARN ╗📛\n\n' + Config.WARN4 + '\n\n\n      ❰🍁🔱  T Rex BOT  🔱🍁❱   '})
 
     }));
 }
@@ -43,7 +43,7 @@ else if (Config.WORKTYPE == 'public') {
 
           
             var image = await axios.get ('https://telegra.ph/file/8f6b8a908ff9e67f7467e.jpg', {responseType: 'arraybuffer'})
-        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: '\n      📛╔ GROUP WARNS ╗📛\n\n' + Config.WARN1 + '\n\n\n      ❰🍁🔱  T Rex BOT  🔱🍁❱   '})
+        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: '\n      📛╔ GROUP WARN ╗📛\n\n' + Config.WARN1 + '\n\n\n      ❰🍁🔱  T Rex BOT  🔱🍁❱   '})
 
     }));
   
@@ -51,21 +51,21 @@ else if (Config.WORKTYPE == 'public') {
 
           
             var image = await axios.get ('https://telegra.ph/file/8f6b8a908ff9e67f7467e.jpg', {responseType: 'arraybuffer'})
-        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: '\n      📛╔ GROUP WARNS ╗📛\n\n' + Config.WARN2 + '\n\n\n      ❰🍁🔱  T Rex BOT  🔱🍁❱   ' })
+        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: '\n      📛╔ GROUP WARN ╗📛\n\n' + Config.WARN2 + '\n\n\n      ❰🍁🔱  T Rex BOT  🔱🍁❱   ' })
 
     }));
   Amdi.applyCMD({pattern: 'warn3', fromMe: true,  deleteCommand: false, onlyGroup: true }, (async (message, match) => {
 
           
             var image = await axios.get ('https://telegra.ph/file/8f6b8a908ff9e67f7467e.jpg', {responseType: 'arraybuffer'})
-        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: '\n      📛╔ GROUP WARNS ╗📛\n\n' + Config.WARN3 + '\n\n\n      ❰🍁🔱  T Rex BOT  🔱🍁❱   ' })
+        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: '\n      📛╔ GROUP WARN ╗📛\n\n' + Config.WARN3 + '\n\n\n      ❰🍁🔱  T Rex BOT  🔱🍁❱   ' })
 
     }));
   Amdi.applyCMD({pattern: 'warn4', fromMe: true,  deleteCommand: false, onlyGroup: true }, (async (message, match) => {
 
           
             var image = await axios.get ('https://telegra.ph/file/8f6b8a908ff9e67f7467e.jpg', {responseType: 'arraybuffer'})
-        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: '\n      📛╔ GROUP WARNS ╗📛\n\n' + Config.WARN4 + '\n\n\n      ❰🍁🔱  T Rex BOT  🔱🍁❱   ' })
+        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: '\n      📛╔ GROUP WARN ╗📛\n\n' + Config.WARN4 + '\n\n\n      ❰🍁🔱  T Rex BOT  🔱🍁❱   ' })
 
     }));
 }
