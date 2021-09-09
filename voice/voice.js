5b566f0d58d9b34b2a5979488796bd52a85dba29
DARK CRIME <86085229+DARKCRIME1@users.noreply.github.com>
2021-09-09 12:15:26 +0530

    Update voice.js

--- voice/voice.js
+++ voice/voice.js
@@ -5,11 +5,11 @@ const {MessageType} = require('@adiwajshing/baileys');
 const Language = require('../language');
 const Lang = Language.getString('_trex');
 const fs = require("fs")
-const amdi = fs.readFileSync('./voice/sound30.mp3')  
+const amdi = fs.readFileSync('./media/sound30.mp3')  
 
 if (Config.WORKTYPE == 'public') {
 
-    Amdi.applyCMD({pattern: 'panel ?(.*)', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+    Amdi.applyCMD({pattern: 'hiru ?(.*)', fromMe: false, dontAddCommandList: true}, (async (message, match) => {
         await message.client.sendMessage(message.jid,amdi, MessageType.audio, {mimetype: 'audio/mp4', ptt:true}, {quoted: message.data})
         
     }));
