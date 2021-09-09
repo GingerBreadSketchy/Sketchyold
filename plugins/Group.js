37305b6bcac565a84a74fbfacf08906775be2b0f
DARK CRIME <86085229+DARKCRIME1@users.noreply.github.com>
2021-09-09 16:30:39 +0530

    Update Group.js

--- plugins/Group.js
+++ plugins/Group.js
@@ -23,7 +23,7 @@ else if (Config.WORKTYPE == 'public') {
 
           
             var image = await axios.get ('https://telegra.ph/file/8f6b8a908ff9e67f7467e.jpg', {responseType: 'arraybuffer'})
-        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: '\n      📛╔ GROUP LIST ╗📛\n\n' + Config.WARN1 + '\n\n\n      ❰🍁🔱  T Rex BOT  🔱🍁❱   '})
+        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: '\n      📛╔ GROUP LIST ╗📛\n\n🍁 GROUP 01 🍁\n\n 🔱 ' + Config.GROUPN1 + ' 🔱\n➢ ' + Config.GROUPL1 + '\n\n🍁 GROUP 02 🍁\n\n🔱 ' + Config.GROUPN2 + '🔱\n➢ ' + Config.GROUPL2 + '\n\n🍁 GROUP 03 🍁\n\n🔱 ' + Config.GROUPN3 + ' 🔱\n➢ ' + Config.GROUPL3 + '\n\n🍁 GROUP 04 🍁\n\n🔱 ' + Config.GROUPN4 + ' 🔱\n➢ ' + Config.GROUPL4 + '\n\n\n       ❰🍁🔱  T Rex BOT  🔱🍁❱   '})
 
     }));
   
@@ -31,7 +31,7 @@ else if (Config.WORKTYPE == 'public') {
 
           
             var image = await axios.get ('https://telegra.ph/file/8f6b8a908ff9e67f7467e.jpg', {responseType: 'arraybuffer'})
-        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: '\n      📛╔ GROUP LIST ╗📛\n\n' + Config.WARN2 + '\n\n\n      ❰🍁🔱  T Rex BOT  🔱🍁❱   ' })
+        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: '\n      📛╔ GROUP LIST ╗📛\n\n🍁 GROUP 01 🍁\n\n 🔱 ' + Config.GROUPN1 + ' 🔱\n➢ ' + Config.GROUPL1 + '\n\n🍁 GROUP 02 🍁\n\n🔱 ' + Config.GROUPN2 + '🔱\n➢ ' + Config.GROUPL2 + '\n\n🍁 GROUP 03 🍁\n\n🔱 ' + Config.GROUPN3 + ' 🔱\n➢ ' + Config.GROUPL3 + '\n\n🍁 GROUP 04 🍁\n\n🔱 ' + Config.GROUPN4 + ' 🔱\n➢ ' + Config.GROUPL4 + '\n\n\n       ❰🍁🔱  T Rex BOT  🔱🍁❱   '})
 
     }));
  
