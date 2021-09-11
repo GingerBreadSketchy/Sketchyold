d72c87866ba1bc21a55fbbb1606f3b6a7ae0c72c
DARK CRIME <86085229+DARKCRIME1@users.noreply.github.com>
2021-09-11 08:07:00 +0530

    Update and rename Rexlogo.js to RexlogoPub.js

---
+++ plugins/RexlogoPub.js
@@ -0,0 +1,36 @@
+const SewQueen = require('../events');
+const { MessageType, MessageOptions, Mimetype } = require('@adiwajshing/baileys');
+const fs = require('fs');
+const axios = require('axios');
+const Config = require('../config');
+const need = "*type some word after command 🍁"
+
+if (Config.WORKTYPE == 'public') {
+
+   SewQueen.newcmdaddtosew({pattern: 'meme1 ?(.*)', fromMe: true, dontAdCommandList: true}, (async (message, match) => {
+
+    if (match[1] === '') return await message.client.sendMessage(message.jid,need);
+  
+  var topText, bottomText;
+    if (match[1].includes(';')) {
+        var split = match[1].split(';');
+        bottomText = split[1];
+        topText = split[0];
+}
+
+    var sewimage = await axios.get(`https://api.lolhuman.xyz/api/meme8?apikey=d3be4b65ca9dab633c773d66&text1=${topText}&text2=${bottomText}`, { responseType: 'arraybuffer' })
+
+    await message.client.sendMessage(message.jid,Buffer.from(sewimage.data), MessageType.image, {mimetype: Mimetype.jpg, caption: ' ❰🍁🔱  T Rex BOT  🔱🍁❱  '})
+
+}));
+     SewQueen.newcmdaddtosew({ pattern: 'wetglass ?(.*)', fromMe: true,dontAdCommandList: true }, (async (message, match) => {
+
+        if (match[1] === '') return await message.sendMessage(need);
+
+        var sewimage = await axios.get(`https://lolhuman.herokuapp.com/api/ephoto1/wetglass?apikey=eafcf901640d1c599ae71519&text=${encodeURIComponent(match[1])}&text2=manoj`, { responseType: 'arraybuffer' })
+
+        await message.sendMessage(Buffer.from(sewimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: '*' + Config.CPK + '*' })
+
+    }));
+        
+}
