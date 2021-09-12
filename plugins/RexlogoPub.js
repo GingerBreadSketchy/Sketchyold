76b566aae470d080e387b9eb384a4359c2211aa5
DARK CRIME <86085229+DARKCRIME1@users.noreply.github.com>
2021-09-12 07:56:23 +0530

    Update RexlogoPub.js

--- plugins/RexlogoPub.js
+++ plugins/RexlogoPub.js
@@ -113,7 +113,7 @@ if (Config.WORKTYPE == 'public') {
       
       var sewimage = await axios.get(`https://api.lolhuman.xyz/api/meme8?apikey=d3be4b65ca9dab633c773d66&text1=${topText}&text2=${bottomText}`, { responseType: 'arraybuffer' })
       
-      await message.client.sendMessage(message.jid,Buffer.from(sewimage.data), MessageType.image, {mimetype: Mimetype.jpg, caption: ' ❰🍁🔱  T Rex BOT  🔱🍁❱  '})
+      await message.client.sendMessage(message.jid,Buffer.from(sewimage.data), MessageType.image, {mimetype: Mimetype.jpg, caption: ' ❰🍁🔱  T Rex BOT  🔱🍁❱  ',quoted: message.data})
    
    }));
    
