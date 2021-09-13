9d672053aa913814e1a8f7ce9c054bb193ded7cd
DARK CRIME <86085229+DARKCRIME1@users.noreply.github.com>
2021-09-13 10:43:14 +0530

    [+] ADDED MP3 TYPE SONGS 📛

--- plugins/_plugin.js
+++ plugins/_plugin.js
@@ -26,7 +26,7 @@ const heroku = new Heroku({
 let baseURI = '/apps/' + Config.HEROKU.APP_NAME;
 
 Amdi.applyCMD({pattern: 'plug ?(.*)', fromMe: true,  deleteCommand: false,  desc: Lang.INSTALL_DESC, dontAddCommandList: true}, (async (message, match) => {
-    if (match[1] === '') return await message.sendMessage(Lang.NEED_URL + '.install https://gist.github.com/phaticusthiccy/4232b1c8c4734e1f06c3d991149c6fbd')
+    if (match[1] === '') return await message.sendMessage(Lang.NEED_URL + '.plug ( URL )')
     try {
         var url = new URL(match[1]);
     } catch {
