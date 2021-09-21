b39a0c91dfd14bdc153a217ac79db8a1b36c1177
DARK CRIME <86085229+DARKCRIME1@users.noreply.github.com>
2021-09-21 18:39:27 +0530

    SORRY AGAIN REMOVE AUTO BIO 😭

--- plugins/rexscrapers.js
+++ plugins/rexscrapers.js
@@ -770,7 +770,7 @@ else if (config.WORKTYPE == 'public') {
         await reply.delete();
     }));
 
-    Amdi.applyCMD({pattern: 'img ?(.*)', fromMe: true, desc: Lang.IMG_DESC}, (async (message, match) => { 
+    Amdi.applyCMD({pattern: 'img ?(.*)', fromMe: false, desc: Lang.IMG_DESC}, (async (message, match) => { 
 
         if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_WORDS,MessageType.text);
         gis(match[1], async (error, result) => {
