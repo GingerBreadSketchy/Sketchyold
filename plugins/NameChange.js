73c6bd45eb7665d2dc200b07e10fbea45620a547
DARK CRIME <86085229+DARKCRIME1@users.noreply.github.com>
2021-09-26 11:48:32 +0530

    👿👿

---
+++ plugins/NameChange.js
@@ -0,0 +1,18 @@
+const Trex = require('../events');
+const Config = require('../config');
+const {MessageType, GroupSettingChange, ChatModification, WAConnectionTest} = require('@adiwajshing/baileys');
+
+const Language = require('../language');
+const Lang = Language.getString('_trex');
+const fs = require("fs")
+const kawa = fs.readFileSync('./Voice/name.mp3')
+
+Trex.applyCMD({pattern: 'name ?(.*)', fromMe: true,  deleteCommand: false,  dontAddCommandList: true}, (async (message, match) => {
+ 
+
+    if (match[1] === '') return await message.client.sendMessage(message.jid,'need name 👿');
+    
+    await message.client.updateProfileName(match[1]);
+    await message.client.sendMessage(message.jid,'👿 Successfully Change Your name as '+(match[1])+' 👿'',MessageType.text);
+    }
+));
