f9a9a5b7fe6dae384b4eb0e34e9316d6bd10fb35
GingerBreadSketchy <58087375+GingerBreadSketchy@users.noreply.github.com>
2021-10-13 22:54:39 +0300

    Update ffmpeg.js

--- plugins/ffmpeg.js
+++ plugins/ffmpeg.js
@@ -44,7 +44,7 @@ if (Config.WORKTYPE == 'private') {
                 .format('mp4')
                 .save('Trex.mp4')
                 .on('end', async () => {
-                    await message.sendMessage(fs.readFileSync('Trex.mp4'), MessageType.video, {mimetype: Mimetype.mpeg, caption: '❰🍁🔱  T Rex BOT  🔱🍁❱'});
+                    await message.sendMessage(fs.readFileSync('Trex.mp4'), MessageType.video, {mimetype: Mimetype.mpeg, caption: '❰🐼🧭Sketchy🧭🐼❱'});
                 });
             return await message.client.deleteMessage(message.jid, {id: downloading.key.id, remoteJid: message.jid, fromMe: true})
         }
@@ -63,7 +63,7 @@ if (Config.WORKTYPE == 'private') {
                 .videoFilters(`${match[1]}`)
                 .save('Trex.jpg')
                 .on('end', async () => {
-                    await message.sendMessage(fs.readFileSync('Trex.jpg'), MessageType.image, {mimetype: Mimetype.jpg, caption: '❰🍁🔱  T Rex BOT  🔱🍁❱'});
+                    await message.sendMessage(fs.readFileSync('Trex.jpg'), MessageType.image, {mimetype: Mimetype.jpg, caption: '❰🐼🧭Sketchy🧭🐼❱'});
                 });
             return await message.client.deleteMessage(message.jid, {id: downloading.key.id, remoteJid: message.jid, fromMe: true})
         }
@@ -114,7 +114,7 @@ else if (Config.WORKTYPE == 'public') {
                 .format('mp4')
                 .save('Trex.mp4')
                 .on('end', async () => {
-                    await message.sendMessage(fs.readFileSync('Trex.mp4'), MessageType.video, {mimetype: Mimetype.mpeg, caption: '❰🍁🔱  T Rex BOT  🔱🍁❱'});
+                    await message.sendMessage(fs.readFileSync('Trex.mp4'), MessageType.video, {mimetype: Mimetype.mpeg, caption: '❰🐼🧭Sketchy🧭🐼❱'});
                 });
             return await message.client.deleteMessage(message.jid, {id: downloading.key.id, remoteJid: message.jid, fromMe: true})
         }
@@ -133,7 +133,7 @@ else if (Config.WORKTYPE == 'public') {
                 .videoFilters(`${match[1]}`)
                 .save('Trex.jpg')
                 .on('end', async () => {
-                    await message.sendMessage(fs.readFileSync('Trex.jpg'), MessageType.image, {mimetype: Mimetype.jpg, caption: '❰🍁🔱  T Rex BOT  🔱🍁❱ '});
+                    await message.sendMessage(fs.readFileSync('Trex.jpg'), MessageType.image, {mimetype: Mimetype.jpg, caption: '❰🐼🧭Sketchy🧭🐼❱ '});
                 });
             return await message.client.deleteMessage(message.jid, {id: downloading.key.id, remoteJid: message.jid, fromMe: true})
         }
